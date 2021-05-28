b922a75ac3230b812d49a8c4019413ab7020ee66
dankelley <kelley.dan@gmail.com>
2021-05-28 15:33:15 -0300

    alphabetize tables in elements vignette

--- vignettes/knockdown_model.R
+++
@@ -1,8 +0,0 @@
-## ----echo=FALSE---------------------------------------------------------------
-library(mooring)
-f <- float("HMB 20")
-w <- wire(length=100)
-
-## ----echo=FALSE,dev.args=list(pointsize=9),fig.cap="Response of a mooring to a $u=1$m/s current, as described in the text. **Left**: Cable tension, in kg, with gray for the $u=0$ case and black for the $u=1$m/s case. **Right:** Mooring shape, with same colour scheme as left panel."----
-source("example_01.R")
-
