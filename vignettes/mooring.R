0fa8f86c71daf5752e96c22d7ea29f348bea0990
dankelley <kelley.dan@gmail.com>
2021-06-04 09:10:22 -0300

    area(), CD() and drag() accept elements (issue 15)
    See https://github.com/dankelley/mooring/issues/15

--- vignettes/mooring.R
+++ vignettes/mooring.R
@@ -24,3 +24,14 @@ par(mfrow=c(1, 2))
 plot(mdk, which="tension", fancy=TRUE)
 plot(mdk, which="shape", fancy=TRUE)
 
+## -----------------------------------------------------------------------------
+i <- instrument("myNewInstrument", buoyancy=-5, height=1, area=0.1, CD=1)
+
+## -----------------------------------------------------------------------------
+w <- wire(length=100) # default wire type
+w2 <- w
+w2$CD <- 2 * w2$CD
+
+## -----------------------------------------------------------------------------
+c(drag(w, 1), drag(w2, 1))
+
