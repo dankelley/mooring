cd51e51b75a2551619d62d7980be0a3ec3dc1830
dankelley <kelley.dan@gmail.com>
2021-06-08 15:27:36 -0300

    add connector() type
    This involves moving several items from chain() types.

---
+++ man-roxygen/lengthTemplate.R
@@ -0,0 +1,5 @@
+#' @param length numeric value for the length (in m) of the element.
+#' This value is used in the computation of (a) the mooring geometry
+#' and (b) the area of the element (which is in turn used in
+#' the drag computation).
+#' Note that `length` is ignored if `model` is recognized.
