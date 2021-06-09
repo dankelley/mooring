a59c92bfcf783e3231b79d62e3cc70f5add70e95
dankelley <kelley.dan@gmail.com>
2021-06-09 08:05:30 -0300

    update findElement(). Start real-mooring test.

--- R/mooring.R
+++ R/mooring.R
@@ -1299,10 +1299,13 @@ buoyancy <- function(m, debug=0L)
 #' [agrep()].  The output (if any) is in the form of suggested calls
 #' to element-creating functions
 #' [anchor()], [chain()], [connector()], [float()], [instrument()], and [wire()].
+#' The list is in alphabetical order, not the order of the closeness of the match.
 #'
 #' @param e character value to be used for the fuzzy match, passed on to [agrep()].
 #'
 #' @param max.distance numeric value passed on to [agrep()].
+#'
+#' @param search character vector holding the categories to be searched for.
 #'
 #' @return `findElement` returns (silently) a list of the suggested function calls.
 #'
@@ -1313,20 +1316,22 @@ buoyancy <- function(m, debug=0L)
 #' @export
 #'
 #' @author Dan Kelley
-findElement <- function(e, max.distance=0.1)
+findElement <- function(e, max.distance=0.1, search=c("anchor", "chain", "connector", "float", "instrument", "wire"))
 {
     data("mooringElements", package="mooring", envir=environment())
     mooringElements <- get("mooringElements")
     rval <- NULL
-    for (element in c("anchors", "chains", "connectors", "floats", "instruments", "wires")) {
-        names <- mooringElements[[element]]$name
-        match <- agrep(e, names, max.distance=max.distance)
-        for (i in seq_along(match)) {
-            rval <- c(rval, paste0(element, "('", names[match], "')"))
+    for (element in search) {
+        names <- mooringElements[[paste0(element,"s")]]$name
+        match <- try(agrep(e, names, max.distance=max.distance), silent=TRUE)
+        if (!inherits(match, "try-error")) {
+            for (i in seq_along(match)) {
+                rval <- c(rval, paste0(element, "('", names[match], "')"))
+            }
         }
     }
     if (is.null(rval)) {
-        cat("Sorry, found no good matches for \"", e, "\".\n")
+        cat("Sorry, found no good matches for \"", e, "\".\n", sep="")
     } else {
         cat("Some possible matches:\n")
         rval <- sort(unique(rval))
--- sandbox/dk/03_BIO_mooring/03_BIO_mooring_01.R
+++ sandbox/dk/03_BIO_mooring/03_BIO_mooring_01.R
@@ -1,12 +1,45 @@
+## Mooring 1840 (p10 of doc)
 library(mooring)
-m <- mooring(anchor(depth=1400),
-             chain("5/8 galvanized chain", length=10),
-             release("dual benthos 965-a")
-             float("3 glass bub"),
-             wire("3/16in jacketed", length=34),
-             instrument("SBE37 microcat"),
-             instrument("RCM11"),
-             wire("3/16in jacketed", length=50),
-             float("3 glass bub"),
-             wire("3/16in jacketed", length=144)) # and more
+# Abbreviations for convenience
+W <- function(length) wire("3/16in galvanized wire coated to 1/4in", length=length)
+BUB3 <- float("streamlined bub 3 Viny balls")
+RCM11 <- instrument("RCM-11 in frame") # "AANDERAA RCM11"
+microcat <- instrument("SBE37 microcat clamp-on style") # "SBE MICROCAT"
 
+m <- mooring(anchor(depth=1400),
+             chain("5/8in galvanized chain", length=10),
+             release("benthos 965a release"),  # dual benthos 965-a
+             BUB3,
+             W(34),
+             microcat,
+             RCM11,
+             W(50),
+             BUB3,
+             W(144),
+             microcat,
+             RCM11,
+             W(147),
+             BUB3,
+             W(46),
+             microcat,
+             RCM11,
+             W(198),
+             microcat,
+             RCM11,
+             W(198),
+             microcat,
+             RCM11,
+             W(197),
+             microcat,
+             RCM11,
+             W(146),
+             microcat,
+             connector("swivel"),
+             connector("ballast", 100/2.2, height=1, width=0.05, CD=1), # guess
+             float("syn. float,bracket and 109lb ADCP"), # total guess; lots of choices
+             W(149),
+             microcat,
+             float("streamlined bub 3 Viny balls")) # total guess
+md <- discretise(m)
+mdk <- knockdown(md, u=function(depth) exp(-depth/1000))
+plot(mdk)
