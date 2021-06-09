c41d6e9401905e704f84bb19a65d2d39b82dc8d3
dankelley <kelley.dan@gmail.com>
2021-06-09 14:32:51 -0300

    update bio float table (thanks, @formulafunracing)

--- R/mooring.R
+++ R/mooring.R
@@ -887,7 +887,7 @@ plot.mooring <- function(x, which="shape",
 #' so that the deformation by a current can be traced more
 #' accurately by [knockdown()].
 #'
-#' @param m an object of the `"mooring"` class.
+#' @template mTemplate
 #'
 #' @param by numeric value giving the rough size of the chunks.
 #' The actual size is computed as the length of wire, divided
@@ -1341,3 +1341,52 @@ findElement <- function(e, max.distance=0.1, search=c("anchor", "chain", "connec
     invisible(rval)
 }
 
+#' Summarize a mooring
+#'
+#' @param object a mooring object, created by [mooring()].
+#'
+#' @param ... ignored.
+#'
+#' @examples
+#' library(mooring)
+#' # Simple case
+#' m <- mooring(anchor(depth=100), wire(length=80), float("HMB 20"))
+#' summary(m)
+#' # Illustrate how it collects wire subintervals
+#' md <- discretise(m)
+#' mdk <- knockdown(md, 0.5)
+#' summary(mdk)
+#'
+#' @export
+#'
+#' @author Dan Kelley
+summary.mooring <- function(object, ...)
+{
+    m <- object # use a more useful name
+    if (!isMooring(m))
+        stop("only works for objects created by mooring()")
+    n <- length(m)
+    lastWasWire <- FALSE
+    wireLength <- 0
+    iWireStart <- 0
+    for (i in seq_len(n)) {
+        mi <- m[[i]]
+        if (inherits(mi, "wire")) {
+            if (!lastWasWire)
+                iWireStart <- i
+            lastWasWire <- TRUE
+            wireLength <- wireLength + mi$height
+        } else {
+            if (lastWasWire) {
+                # fake an element (and blank out the location)
+                W <- m[[iWireStart]]
+                W$height <- wireLength
+                #cat("iWireStart=", iWireStart, "\n")
+                print(W)
+            }
+            lastWasWire <- FALSE
+            wireLength <- 0
+            print(mi)
+        }
+    }
+}
