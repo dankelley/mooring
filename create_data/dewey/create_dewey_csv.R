a6a7628fdd8626ae1150084879ee526d5a0d93e2
dankelley <kelley.dan@gmail.com>
2021-06-21 17:54:03 -0300

    fix zero CD values for 2 elements

--- create_data/dewey/create_dewey_csv.R
+++ create_data/dewey/create_dewey_csv.R
@@ -37,15 +37,13 @@ fixnames <- function(names)
     names <- gsub("8' ", "8ft ", names)
     # Not sure whether the 1/2 in Dyneema is a width, so for caution change back.
     names <- gsub("1/2in Dyneema", "1/2 Dyneema", names)
-    print(data.frame(orig=orig, later=names))
+    #print(data.frame(orig=orig, later=names))
     names
 }
 
 #> Use next to count spaces
 #> count <- "      0.........1........2.........3.........4.........5.\n"
 #> print(head(d$floats, 3))
-#> cat(count, sep="")
-cat("# Floats\n")
 floats <- read.fwf(textConnection(d$floats),widths=c(18,7,6,6,6,5,4), col.names=names)
 originalName <- floats$name
 floats$name <- fixnames(floats$name)
@@ -54,9 +52,12 @@ floats$width <- NULL # this is always 0, and we don't use it in this package
 floats$diameter <- floats$diameter / 100
 floats <- cbind(floats, source="Dewey")
 floats <- cbind(floats, originalName=trimws(originalName))
+for (w in which(0 == floats$CD)) {
+    floats$CD[w] <- median(floats$CD)
+    warning("Float '", floats$name[w], "': changed CD from 0 to ", floats$CD[w], sep="")
+}
 write.csv(floats, "floats_dewey.csv", row.names=FALSE)
 
-cat("# Wires\n")
 # Wires. Dewey lists all diameters as zero, as a way to decode things
 # later (I guess, to distinguish from floats) but we are not trying
 # to make tidy data and there's no need for such tricks, so we call
@@ -73,10 +74,13 @@ n[n == "buoyancy"] <- "buoyancyPerMeter"
 names(wires) <- n
 wires <- cbind(wires, source="Dewey")
 wires <- cbind(wires, originalName=trimws(originalName))
+for (w in which(0 == wires$CD)) {
+    wires$CD[w] <- median(wires$CD)
+    warning("Wire '", wires$name[w], "': changed CD from 0 to ", wires$CD[w], sep="")
+}
 write.csv(wires, "wires_dewey.csv", row.names=FALSE)
 
 # Chains and connectors
-cat("# Chains and connectors\n")
 # I call things as 'chain' if they are used in variable lengths (and then I
 # store buoyancyPerMeter, kg/m) and 'connector' otherwise (and then I store
 # buoyancy, kg).
@@ -94,6 +98,10 @@ chains$diameter <- NULL # is 0 anyway
 chains$name <- fixnames(chains$name)
 chains <- cbind(chains, source="Dewey")
 chains <- cbind(chains, originalName=trimws(originalName[isChain]))
+for (w in which(0 == chains$CD)) {
+    chains$CD[w] <- median(chains$CD)
+    warning("Chain '", chains$name[w], "': changed CD from 0 to ", chains$CD[w], sep="")
+}
 write.csv(chains, "chains_dewey.csv", row.names=FALSE)
 
 connectors <- cc[!isChain, ]
@@ -103,6 +111,10 @@ connectors$height <- connectors$height / 100
 connectors$width <- connectors$width / 100
 connectors <- cbind(connectors, source="Dewey")
 connectors <- cbind(connectors, originalName=trimws(originalName[!isChain]))
+for (w in which(0 == connectors$CD)) {
+    connectors$CD[w] <- median(connectors$CD)
+    warning("Connector '", connectors$name[w], "': changed CD from 0 to ", connectors$CD[w], sep="")
+}
 write.csv(connectors, "connectors_dewey.csv", row.names=FALSE)
 
 
