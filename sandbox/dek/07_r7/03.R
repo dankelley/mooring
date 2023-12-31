5160d74c88164fabe67f7235e93e555466c13789
dankelley <kelley.dan@gmail.com>
2023-12-31 14:52:52 -0400

    more S7 tests

---
+++ sandbox/dek/07_r7/03.R
@@ -0,0 +1,46 @@
+library(S7)
+mooring <- new_class("mooring",
+    package = "mooring",
+    properties = list(
+        elements = class_list, # holds mooringElement items
+        waterDepth = class_numeric
+    ),
+    constructor = function(..., waterDepth=NA_real_) {
+        #cat("in mooring constructor\n")
+        elements <- list(...)
+        #print(elements[[1]])
+        if (!inherits(elements[[1]], "mooring::anchor")) stop("element 1 is not an anchor")
+        if (is.na(waterDepth)) {
+            waterDepth <- elements[[1]]@waterDepth
+        } else {
+            message("anchor depth takes precedence")
+        }
+        new_object(S7_object(), elements=elements, waterDepth=waterDepth)
+    }
+)
+
+mooringElement <- new_class("mooringElement",
+    package = "mooring",
+    properties = list(
+        model = class_character,
+        buoyancy = class_numeric,
+        height = class_numeric,
+        area = class_numeric,
+        CD = class_numeric,
+        source = class_character,
+        originalName = class_character
+    )
+)
+float <- new_class("float",
+    parent = mooringElement,
+    package = "mooring"
+)
+anchor <- new_class("anchor",
+    parent = mooringElement,
+    package = "mooring",
+    properties = list(waterDepth = class_numeric)
+)
+#f <- float()
+#m <- mooring()
+#f
+#message("FIXME: add constructors")
---
+++ sandbox/dek/07_r7/04.R
@@ -0,0 +1,25 @@
+source("03.R")
+source("float_test.R")
+source("anchor_test.R")
+testthat::expect_error(mooring(a = 1, b = 2), "element 1 is not an anchor")
+float_test()
+float_test("?")
+f <- float_test(model = "testFloat", buoyancy = 3, height = 0.2, area = 0.04, CD = 1.3)
+str(f)
+# dput(f)
+f@buoyancy
+# buoyancy(f)
+class(f)
+anchor_test(depth = 100)
+# mooring::drag
+# mooring::mooring
+
+a <- anchor_test(depth = 999)
+f <- float_test()
+mooring(a, f)
+
+message("FIXME to do list:")
+message(" * add wire()")
+message(" * add connector()")
+message(" * add misc()")
+message(" * add some other things, I bet")
